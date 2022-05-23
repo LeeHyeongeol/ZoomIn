44d9ce1a87dd2627ca4dc35c27da92623867a803
hyeongirlife <leehyeongul@gmail.com>
2022-05-23 17:42:16 +0900

    [Back]_Feature 웹소켓 설치 및 프로토콜 생성

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http"
+import WebSocket from "ws"
 
 const app = express();
 
@@ -8,12 +10,20 @@ app.set("view engine", "pug")
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 app.get("/", (req, res) =>
   res.render("home")
 )
+const handleListen = () => console.log("Listening on ws://localhost:3000")
+app.listen(3000, handleListen);
 
-app.listen(3000);
+//http와 wss 서버를 둘 다 실행시킬 수 있다.
+//함께 만들기 원한다면 이렇게 하고 아니라면 하나만 해도 됨!
+// const server = http.createServer(app);
+const wss = new WebSocket.Server()
+
+server.listen(3000, handleListen);
 
 
 
